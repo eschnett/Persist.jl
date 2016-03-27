1c8dda7121c18b816fc7792fa573cd7429c537eb
Erik Schnetter <schnetter@gmail.com>
2016-03-26 22:00:30 -0400

    Support PBS

--- src/Persist.jl
+++ src/Persist.jl
@@ -6,7 +6,7 @@ module Persist
 import Base: serialize, deserialize, isready, wait, fetch
 export serialize, deserialize, isready, wait, fetch
 
-export JobManager, ProcessManager, SlurmManager
+export JobManager, ProcessManager, PBSManager, SlurmManager
 export JobStatus, job_empty, job_queued, job_running, job_done, job_failed
 export status, jobinfo, cancel, getstdout, getstderr, cleanup
 export persist, @persist, readmgr
@@ -364,6 +364,190 @@ end
 
 
 
+"PBS job manager: A job manager using the PBS queuing system"
+type PBSManager <: JobManager
+    jobname::AbstractString
+    jobid::AbstractString
+
+    function PBSManager(jobname::AbstractString)
+        new(jobname, "")
+    end
+
+    PBSManager(::Base.SerializationState) = new()
+end
+
+function serialize(s::Base.SerializationState, mgr::PBSManager)
+    Base.Serializer.serialize_type(s, PBSManager)
+    serialize(s, mgr.jobname)
+    serialize(s, mgr.jobid)
+end
+
+function deserialize(s::Base.SerializationState, ::Type{PBSManager})
+    mgr = PBSManager(s)
+    mgr.jobname = deserialize(s)
+    mgr.jobid = deserialize(s)
+    mgr
+end
+
+"Submit a job"
+function submit(job, mgr::PBSManager; usempi::Bool=false, nprocs::Integer=0)
+    @assert nprocs >= 0
+    @assert status(mgr) == job_empty
+    # Create job directory
+    jobdir = jobdirname(mgr.jobname)
+    jobdir = abspath(jobdir)
+    try
+        mkdir(jobdir)
+    catch
+        # There is another job with the same name
+        error("Job directory \"$jobdir\"exists already")
+    end
+    # Serialize the Julia function
+    jobfile = jobfilename(mgr.jobname)
+    open(joinpath(jobdir, jobfile), "w") do f
+        serialize(f, job)
+    end
+    # Create a wrapper script
+    resultfile = resultfilename(mgr.jobname)
+    outfile = outfilename(mgr.jobname)
+    errfile = errfilename(mgr.jobname)
+    shellfile = shellfilename(mgr.jobname)
+    open(joinpath(jobdir, shellfile), "w") do f
+        shellcmd = AbstractString[]
+        if usempi
+            push!(shellcmd, "mpiexec")
+            if nprocs>0
+                push!(shellcmd, "-n", "$nprocs")
+            end
+        end
+        append!(shellcmd, Base.julia_cmd().exec)
+        if !usempi
+            if nprocs>0
+                push!(shellcmd, "-p", "$nprocs")
+            end
+        end
+        juliacmd = "using Persist; Persist.runjob($(juliaquote(jobfile)), $(juliaquote(resultfile)))"
+        push!(shellcmd, "-e", juliacmd)
+        shellcmd = map(shellquote, shellcmd)
+        push!(shellcmd, "</dev/null")
+        push!(shellcmd, ">$(shellquote(outfile))")
+        push!(shellcmd, "2>$(shellquote(errfile))")
+        print(f, """
+#! /bin/sh
+# This is an auto-generated Julia script for the Persist package
+hostname
+$(join(shellcmd, " "))
+""")
+    end
+    # Pre-create output files
+    open(joinpath(jobdir, outfile), "w") do f end
+    open(joinpath(jobdir, errfile), "w") do f end
+    # Start the job in the job directory
+    # TODO: Teach Julia how to use the nodes that PBS reserved
+    buf = readall(setenv(`qsub -D $jobdir -N $(mgr.jobname) -l nodes=$nprocs $shellfile`,
+                         dir=jobdir))
+    m = match(r"([0-9]+)[.]", buf)
+    mgr.jobid = m.captures[1]
+    info("PBS job id: $(mgr.jobid)")
+    # Serialize the manager
+    mgrfile = mgrfilename(mgr.jobname)
+    open(joinpath(jobdir, mgrfile), "w") do f
+        serialize(f, mgr)
+    end
+    nothing
+end
+
+"Get job status code"
+function status(mgr::PBSManager)
+    if isempty(mgr.jobid) return job_empty end
+    try
+        buf = readall(`qstat -x $(mgr.jobid)`)
+        state = chomp(buf)
+        if contains(state, "<job_state>Q</job_state>")
+            return job_queued
+        elseif (contains(state, "<job_state>H</job_state>") ||
+                contains(state, "<job_state>R</job_state>"))
+            return job_running
+        elseif contains(state, "<job_state>C</job_state>")
+            return job_done
+        else
+            @assert false
+        end
+    end
+    # PBS knows nothing about this job
+    resultfile = joinpath(jobdirname(mgr.jobname), resultfilename(mgr.jobname))
+    isfile(resultfile) && job_done
+    job_failed
+end
+
+"Get job info string"
+function jobinfo(mgr::PBSManager)
+    st = status(mgr)
+    @assert st != job_empty
+    try
+        return readall(`qstat $(mgr.jobid)`)
+    end
+    # PBS knows nothing about this job
+    "[job_done]"
+end
+
+"Cancel a job"
+function cancel(mgr::PBSManager; force::Bool=false)
+    @assert status(mgr) != job_empty
+    # TODO: Handle things differently for force=false and force=true
+    run(`qdel $(mgr.jobid)`)
+    nothing
+end
+
+"Check whether a job is done"
+function isready(mgr::PBSManager)
+    return status(mgr) == job_done
+end
+
+"Wait until a job is done"
+function wait(mgr::PBSManager)
+    @assert status(mgr) != job_empty
+    while !(status(mgr) in (job_done, job_failed))
+        sleep(1)
+    end
+    nothing
+end
+
+"Fetch job result"
+function fetch(mgr::PBSManager)
+    wait(mgr)
+    resultfile = joinpath(jobdirname(mgr.jobname), resultfilename(mgr.jobname))
+    local result
+    open(resultfile, "r") do f
+        result = deserialize(f)
+    end
+    result
+end
+
+"Get stdout from a job"
+function getstdout(mgr::PBSManager)
+    @assert status(mgr) != job_empty
+    # TODO: Read stdout while job is running
+    readall(joinpath(jobdirname(mgr.jobname), outfilename(mgr.jobname)))
+end
+
+"Get stderr from a job"
+function getstderr(mgr::PBSManager)
+    @assert status(mgr) != job_empty
+    # TODO: Read stdout while job is running
+    readall(joinpath(jobdirname(mgr.jobname), errfilename(mgr.jobname)))
+end
+
+"Clean up after a job (delete all traces of the job, including its result)"
+function cleanup(mgr::PBSManager)
+    @assert status(mgr) in (job_done, job_failed)
+    rmtree(jobdirname(mgr.jobname))
+    mgr.jobid = ""
+    nothing
+end
+
+
+
 "Slurm job manager: A job manager using the Slurm queuing system"
 type SlurmManager <: JobManager
     jobname::AbstractString
@@ -466,7 +650,7 @@ function status(mgr::SlurmManager)
         if state in ["CF", "PD"]
             return job_queued
         elseif state in ["CG", "R", "S"]
-            return jo_running
+            return job_running
         elseif state in ["CA", "CD", "F", "NF", "PR", "TO"]
             return job_done
         else
